2ec3b638791cf5713c77b2d8430894856929fc15
ubnas1 <116352196+ubnas1@users.noreply.github.com>
2023-02-21 17:36:03 +1100

    Add files via upload

---
+++ crowdfunding_db.sql
@@ -0,0 +1,92 @@
+-- -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- -- Link to schema: https://app.quickdatabasediagrams.com/#/d/VeVfDb
+-- -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+-- CREATE TABLE "campaign" (
+--     "id" serial   NOT NULL,
+--     "cf_id" int   NOT NULL,
+--     "contact_id" int   NOT NULL,
+--     "company_name" varchar(50)   NOT NULL,
+--     "description" varchar(100)   NOT NULL,
+--     "goal" int   NOT NULL,
+--     "pledged" int   NOT NULL,
+--     "outcome" varchar(10)   NOT NULL,
+--     "backers_count" int   NOT NULL,
+--     "country" varchar(10)   NOT NULL,
+--     "currency" varchar(10)   NOT NULL,
+--     "launch_date" date   NOT NULL,
+--     "end_date" date   NOT NULL,
+--     "category_id" varchar(10)   NOT NULL,
+--     "subcategory_id" varchar(10)   NOT NULL,
+--     CONSTRAINT "pk_campaign" PRIMARY KEY (
+--         "id"
+--      ),
+--     CONSTRAINT "uc_campaign_contact_id" UNIQUE (
+--         "contact_id"
+--     )
+-- );
+
+-- CREATE TABLE "category" (
+--     "id" serial   NOT NULL,
+--     "category_id" varchar(10)   NOT NULL,
+--     "category" varchar(30)   NOT NULL,
+--     CONSTRAINT "pk_category" PRIMARY KEY (
+--         "id"
+--      ),
+--     CONSTRAINT "uc_category_category_id" UNIQUE (
+--         "category_id"
+--     ),
+--     CONSTRAINT "uc_category_category" UNIQUE (
+--         "category"
+--     )
+-- );
+
+-- CREATE TABLE "subcategory" (
+--     "id" serial   NOT NULL,
+--     "subcategory_id" varchar(10)   NOT NULL,
+--     "subcategory" varchar(30)   NOT NULL,
+--     CONSTRAINT "pk_subcategory" PRIMARY KEY (
+--         "id"
+--      ),
+--     CONSTRAINT "uc_subcategory_subcategory_id" UNIQUE (
+--         "subcategory_id"
+--     ),
+--     CONSTRAINT "uc_subcategory_subcategory" UNIQUE (
+--         "subcategory"
+--     )
+-- );
+
+-- CREATE TABLE "contacts" (
+--     "id" serial   NOT NULL,
+--     "contact_id" int   NOT NULL,
+--     "first_name" varchar(30)   NOT NULL,
+--     "last_name" varchar(30)   NOT NULL,
+--     "email" varchar(50)   NOT NULL,
+--     CONSTRAINT "pk_contacts" PRIMARY KEY (
+--         "id"
+--      ),
+--     CONSTRAINT "uc_contacts_contact_id" UNIQUE (
+--         "contact_id"
+--     ),
+--     CONSTRAINT "uc_contacts_email" UNIQUE (
+--         "email"
+--     )
+-- );
+
+-- ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+-- REFERENCES "category" ("category_id");
+
+-- ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+-- REFERENCES "subcategory" ("subcategory_id");
+
+-- ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
+-- REFERENCES "campaign" ("contact_id");
+
+-- SELECT * FROM campaign;
+
+-- select * from category;
+
+-- select * from contacts;
+
+-- select * from subcategory;
